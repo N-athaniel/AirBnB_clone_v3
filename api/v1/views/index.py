fdef2b7b9285eae2ed4adf15a13f6beb52eda809
fatima-elfat <ffzelfatehy@gmail.com>
2024-01-08 21:42:34 +0100

    add documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
 """
 flask with general routes
-    routes:
-        /status:    display "status":"OK"
 """
 
 from api.v1.views import app_views
 from flask import jsonify
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 from models import storage
 
 
 classes = {
-    "amenities": Amenity,
-    "cities": City,
-    "places": Place,
-    "reviews": Review,
-    "states": State,
-    "users": User,
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User",
 }
 
 
