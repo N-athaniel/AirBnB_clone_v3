eeda314c07b1767e9311da8afde38b8b0443e9fd
fatima-elfat <ffzelfatehy@gmail.com>
2024-01-08 21:05:49 +0100

    initilize list

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,6 +6,7 @@ using flask blueprint factor an application into a set of blueprints.
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from api.v1.views.index import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
